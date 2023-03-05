3bdc688517ddef0ff15b1aa3f302282aa73dfb19
ydhdggd <112242233+ydhdggd@users.noreply.github.com>
2023-03-05 08:00:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -135,16 +135,10 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
+        caption = FORCE_MSG.format(message.from_user.mention),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
-        disable_web_page_preview = True
+        #disable_web_page_preview = True
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
