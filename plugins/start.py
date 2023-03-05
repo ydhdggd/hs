c9ff79d867ded8937bf78a1797d066f671e21a2d
ydhdggd <112242233+ydhdggd@users.noreply.github.com>
2023-03-05 08:47:17 +0530

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
@@ -95,7 +95,13 @@ async def start_command(client: Client, message: Message):
         )
         await message.reply_photo(
             photo="https://te.legra.ph/file/403a8223288699d50ecf1.jpg",
-            caption = f"Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.",
+            caption = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
             reply_markup = reply_markup,
             #disable_web_page_preview = True,
             quote = True
@@ -134,12 +140,17 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply_photo(
-        photo="https://te.legra.ph/file/403a8223288699d50ecf1.jpg",
-        caption = f"Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel",
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
-        #disable_web_page_preview = True
+        disable_web_page_preview = True
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
